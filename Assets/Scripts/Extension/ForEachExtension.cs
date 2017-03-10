be5650f23fb21cce42a893dbdf55a095ef933762
Crovax9 <Crovax9@gmail.com>
2017-03-10 18:13:23 +0900

    Prefabs Add
    Maps & Obstacles Prefabs Add

---
+++ Assets/Scripts/Extension/ForEachExtension.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public static class ForEachExtension {
+    public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, 
+        Action<T> action)
+    {
+        foreach (T item in source)
+            action(item);
+
+        return source;
+    }
+	
+    public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source,
+        Action<T, int> action)
+    {
+        int index = 0;
+        foreach (T item in source)
+            action(item, index++);
+
+        return source;
+    }
+}
