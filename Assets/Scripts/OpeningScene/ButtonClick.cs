08091d047382f72ae9320e9271bce13583729351
Crovax9 <Crovax9@gmail.com>
2017-04-18 21:07:39 +0900

    OptionWindow Add & TweenPosition Add
    OptionWindow & toggle add
    TweenPosition Add
    ButtonClick Method Add

--- Assets/Scripts/OpeningScene/ButtonClick.cs
+++ Assets/Scripts/OpeningScene/ButtonClick.cs
@@ -3,18 +3,25 @@ using System.Collections;
 
 public class ButtonClick : MonoBehaviour
 {
+    [SerializeField]
+    private TweenPosition rankTween;
+    [SerializeField]
+    private TweenPosition optionTween;
+
     public void StartClick()
     {
         GameManager.Instance.SceneMove("Main");
     }
 
-    void RankClick()
+    public void RankButtonClick()
     {
-
+        rankTween.PlayReverse();
+        optionTween.PlayReverse();
     }
 
-    void OptionClick()
+    public void OptionButtonClick()
     {
-
+        rankTween.PlayForward();
+        optionTween.PlayForward();
     }
 }
